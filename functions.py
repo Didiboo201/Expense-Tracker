7b757e887a1eaea3c7d076e14f3bca99d10b15ed
Didiboo201 <dienab.michel@gmail.com>
2024-11-29 18:36:40 +0100

    Added all main functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+
+import os
+
+def add_expense():
+    """"""
+
+def view_all_expenses():
+    """"""
+
+def view_expenses_by_category():
+    """"""
+
+def calculate_expenses():
+    """"""
+
+def load_file():
+    """"""
+
+def save_file():
+    """"""
+
+def show_menu():
+    """"""
