f93529da36d582a36c6ded7391ffac180fb84785
Didiboo201 <dienab.michel@gmail.com>
2024-11-29 17:50:48 +0100

    Added new files and data structures

---
+++ data_structure.py
@@ -0,0 +1,7 @@
+
+#Data structure to store expenses
+expense_data = {'category1' : 'amount1',
+                'category2' : 'amount2',
+                'category3' : 'amount3'}
+
+individual_expense = ['category' , 'amount', 'description']
---
+++ main.py
@@ -0,0 +1,8 @@
+
+#Users can input the expense amount, category, and a short description.
+
+#View all recorded expenses or filter them by category
+
+#Expenses are saved in a file and automatically loaded when the program starts.
+
+#Option to calculate total spending or spending by category.
