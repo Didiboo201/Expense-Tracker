85b6731e30b1e6a3d0483d777cbea235dd39b303
Didiboo201 <dienab.michel@gmail.com>
2025-01-30 12:28:03 +0100

    Commented data structure

--- data_structure.py
+++ data_structure.py
@@ -1,9 +1,11 @@
 
 #Data structure to store expenses
+"""
 expense_data = {'category1' : 'individual_expense1',
                 'category2' : 'individual_expense2',
                 'category3' : 'individual_expense3'}
 
 individual_expense = ['date' , 'amount', 'description']
+"""
 
 #All those data will be stocked in the "expenses.txt" file
